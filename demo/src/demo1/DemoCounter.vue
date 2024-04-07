78372652429ec7c3c24d0c9541cd1d9b5271c070
Denis M <the.makushin@yandex.ru>
2024-04-07 15:06:03 +0300

    Изменения по второму разделу согласно туториалам

---
+++ demo/src/demo1/DemoCounter.vue
@@ -0,0 +1,21 @@
+<template>
+  <demo-counter>
+    <div class="counter">
+      <counter-button @click="decrement"> - </counter-button>
+      <counter-value :value="count"/>
+      <counter-button @click="increment"> + </counter-button>
+    </div>
+  </demo-counter>
+</template>
+
+<script setup>
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
+</script>
+
+<style lang="scss" scoped>
+.counter {
+  display: flex;
+  align-items: center;
+}
+</style>
