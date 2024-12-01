06a074e12eb6fe7c34df5bac1f0a02fc3511502d
Denis M <the.makushin@yandex.ru>
2024-12-01 23:36:37 +0300

    Добавлена аутентификация

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,9 @@ import { clickOutside } from './common/directives'
 import App from "./App.vue";
 import router from "./router";
 
+import { getToken, removeToken } from '@/services/token-manager'
+import { useAuthStore } from '@/stores'
+
 const app = createApp(App);
 
 app.use(createPinia());
@@ -12,3 +15,16 @@ app.use(router);
 app.directive('click-outside', clickOutside)
 
 app.mount("#app");
+
+// Проверка, если пользователь уже зашел в систему
+const token = getToken()
+if (token) {
+  try {
+    const authStore = useAuthStore()
+    await authStore.getMe()
+    await router.push('/')
+  } catch (e) {
+    removeToken()
+    console.log(e)
+  }
+}
